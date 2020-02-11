c4be6fde16e112168b19bab42113b078eb6de298
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-12 07:14:40 +0900

    First commit

---
+++ Server/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
+app.get('/', (req, res) => res.send('API is running'))
+app.listen(PORT, () => console.log(`Server is online: Running on port ${PORT}`));
