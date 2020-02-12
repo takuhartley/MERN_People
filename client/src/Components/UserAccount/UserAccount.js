6968616305e37d3a6dc320124b41718195097845
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-12 09:56:21 +0900

    Adding content

---
+++ client/src/Components/UserAccount/UserAccount.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UserAccount extends Component {
+    render() {
+        return (
+            <div>
+                <h1>User Account</h1>
+            </div>
+        )
+    }
+}
+
+export default UserAccount
