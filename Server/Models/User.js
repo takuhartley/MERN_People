1cac93cfa91083503723e66975e1866aa7a651ae
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-12 09:34:31 +0900

    Adding more server logic

---
+++ Server/Models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  firstName: {
+    type: String,
+    required: [true, "First name is required"]
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required"]
+  },
+  userName: {
+    type: String,
+    required: [true, "Username is required"],
+    unique
+  },
+  description: {
+    type: String,
+    min: [3, "Who tf are you?"]
+  },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", User);
---
+++ client/src/Components/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login
