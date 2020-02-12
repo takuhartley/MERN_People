6faaaefaccb665a58e48e663b0cb4ff88fb98719
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-12 09:50:52 +0900

    Adding logic to log/reg

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Login extends Component {
-    render() {
-        return (
-            <div>
-                <form>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+
+          <label>Username: </label>
+          <input type="text" value="Username or Email" />
+
+          <label>Password: </label>
+          <input type="text" />
+
+          <label>Re-enter Password: </label>
+          <input type="text" />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
---
+++ client/src/Components/Registeration/Registeration.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export class Registeration extends Component {
+  render() {
+    return (
+      <div>
+        <label>First Name: </label>
+        <input type="text" />
+
+        <label>Last Name: </label>
+        <input type="text" />
+
+        <label>Email: </label>
+        <input type="text" value="Email" />
+
+        <label>Password: </label>
+        <input type="text" />
+
+        <label>Re-enter Password: </label>
+        <input type="text" />
+
+        <input type="submit" value="Create Account" />
+      </div>
+    );
+  }
+}
+
+export default Registeration;
